ef4fc1f2fe1356a92fc57b3bc4639ed26e33015f
al-delta <al-delta@aldelta-To-be-filled-by-O-E-M>
2018-06-09 15:27:23 +0530

    JavaClass

--- app/src/main/java/govirtual360/numberverify/GitPushDemo.java
+++ app/src/main/java/govirtual360/numberverify/GitPushDemo.java
@@ -3,9 +3,6 @@ package govirtual360.numberverify;
 public class GitPushDemo {
     int jsgf = 32;
 
-    public int getJsgf() {
-        return jsgf;
-    }
 
     public void setJsgf(int jsgf) {
         this.jsgf = jsgf;
